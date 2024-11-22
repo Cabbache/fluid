4c23a4c307a2cf578e5d8bc8a9f3013b3d28e1dd
Cabbache <cabbache@cabbache.net>
2024-11-22 01:57:47 +0100

    fmt

--- fluid.cpp
+++ fluid.cpp
@@ -2,9 +2,9 @@
 #include <SDL2/SDL_image.h>
 #include <SDL2/SDL_timer.h>
 
-#include <iostream>
 #include <cmath>
 #include <immintrin.h>
+#include <iostream>
 
 #include <cassert>
 
@@ -26,77 +26,93 @@ void updateBuffer(uint32_t *buffer, int width, int height, int tick) {
 }
 
 struct Vector2 {
-	float x,y;
-	Vector2(float _x = 0, float _y = 0): x(_x), y(_y) {}
-	Vector2 operator+(const Vector2& other) const { return Vector2(x+other.x,y+other.y); }
-	Vector2 operator-(const Vector2& other) const { return Vector2(x-other.x,y-other.y); }
-	friend ostream& operator<<(ostream& os, const Vector2& vec) {
-			os << "(" << vec.x << ", " << vec.y << ")";
-			return os;
+	float x, y;
+	Vector2(float _x = 0, float _y = 0) : x(_x), y(_y) {}
+	Vector2 operator+(const Vector2 &other) const {
+		return Vector2(x + other.x, y + other.y);
+	}
+	Vector2 operator-(const Vector2 &other) const {
+		return Vector2(x - other.x, y - other.y);
+	}
+	friend ostream &operator<<(ostream &os, const Vector2 &vec) {
+		os << "(" << vec.x << ", " << vec.y << ")";
+		return os;
 	}
 };
-inline Vector2 operator*(float s, const Vector2 &vec) { return Vector2(vec.x*s, vec.y*s); }
-inline Vector2 operator*(const Vector2 &vec, float s) { return Vector2(vec.x*s, vec.y*s); }
-inline Vector2 operator/(float s, const Vector2 &vec) { return Vector2(vec.x/s, vec.y/s); }
-inline Vector2 operator/(const Vector2 &vec, float s) { return Vector2(vec.x/s, vec.y/s); }
+inline Vector2 operator*(float s, const Vector2 &vec) {
+	return Vector2(vec.x * s, vec.y * s);
+}
+inline Vector2 operator*(const Vector2 &vec, float s) {
+	return Vector2(vec.x * s, vec.y * s);
+}
+inline Vector2 operator/(float s, const Vector2 &vec) {
+	return Vector2(vec.x / s, vec.y / s);
+}
+inline Vector2 operator/(const Vector2 &vec, float s) {
+	return Vector2(vec.x / s, vec.y / s);
+}
 
 void gradient(Vector2 *output, float *input, uint W, uint H) {
-	for (uint x = 1;x < W-1;++x)
-	for (uint y = 1;y < H-1;++y) {
-		output[x*H+y] = Vector2(
-			input[(x+1)*H+y] - input[(x-1)*H+y],
-			input[x*H+y+1] - input[x*H+y-1]
-		)/2;
-	}
+	for (uint x = 1; x < W - 1; ++x)
+		for (uint y = 1; y < H - 1; ++y) {
+			output[x * H + y] =
+				Vector2(input[(x + 1) * H + y] - input[(x - 1) * H + y],
+						input[x * H + y + 1] - input[x * H + y - 1]) /
+				2;
+		}
 }
 
 void subtract(Vector2 *dst, Vector2 *src, uint W, uint H) {
-	for (uint i = 0;i < W*H;++i)
+	for (uint i = 0; i < W * H; ++i)
 		dst[i] = dst[i] - src[i];
 }
 
 class PhyBox {
-	Vector2 *v; //velocity
-	float *p; //pressure
-	float viscosity=1;
+	Vector2 *v; // velocity
+	float *p;	// pressure
+	float viscosity = 1;
 
-	uint W,H;
-	
-public:
+	uint W, H;
+
+  public:
 	PhyBox(uint width, uint height) : W(width), H(height) {
-		v = new Vector2[W*H];
-		p = new float[W*H];
+		v = new Vector2[W * H];
+		p = new float[W * H];
 	}
 
-	void forward(float dt=1) {
-		Vector2 *adv_res = new Vector2[W*H];
+	void forward(float dt = 1) {
+		Vector2 *adv_res = new Vector2[W * H];
 		advect(adv_res, dt);
-		memcpy(v,adv_res,W*H*sizeof(Vector2));
+		memcpy(v, adv_res, W * H * sizeof(Vector2));
 		delete[] adv_res;
 
 		diffuse(dt);
 		updatePressure();
 
-		Vector2 *pressure_gradient = new Vector2[W*H];
-		gradient(pressure_gradient,p,W,H);
-		subtract(v,pressure_gradient,W,H);
+		Vector2 *pressure_gradient = new Vector2[W * H];
+		gradient(pressure_gradient, p, W, H);
+		subtract(v, pressure_gradient, W, H);
 		delete[] pressure_gradient;
 	}
 
 	uint width() { return W; }
 	uint height() { return H; }
 
-private:
+  private:
 	template <typename T>
-	void jacobi(T *x, T *b, float alpha, float beta, uint iters=20) {
-		//TODO pass memory in arg instead of allocating here
-		T* result = new T[W*H];
-		for (uint t = 0;t < iters;++t) {
-			for (uint i = 1;i < W-1;++i)
-			for (uint j = 1;j < H-1;++j) {
-				result[i*H+j] = (x[(i-1)*H+j]+x[(i+1)*H+j]+x[i*H+j-1]+x[i*H+j+1] + alpha*b[i*H+j])/beta;
-			}
-			memcpy(x, result, sizeof(T)*W*H);
+	void jacobi(T *x, T *b, float alpha, float beta, uint iters = 20) {
+		// TODO pass memory in arg instead of allocating here
+		T *result = new T[W * H];
+		for (uint t = 0; t < iters; ++t) {
+			for (uint i = 1; i < W - 1; ++i)
+				for (uint j = 1; j < H - 1; ++j) {
+					result[i * H + j] =
+						(x[(i - 1) * H + j] + x[(i + 1) * H + j] +
+						 x[i * H + j - 1] + x[i * H + j + 1] +
+						 alpha * b[i * H + j]) /
+						beta;
+				}
+			memcpy(x, result, sizeof(T) * W * H);
 		}
 		delete[] result;
 	}
@@ -115,36 +131,40 @@ private:
 		float dx = index.x - xl;
 		float dy = index.y - yl;
 
-		Vector2 myl = dx*v[xh*H+yl] + (1-dx)*v[xl*H+yl];
-		Vector2 myh = dx*v[xh*H+yh] + (1-dx)*v[xl*H+yh];
-		return dy*myh + (1-dy)*myl;
+		Vector2 myl = dx * v[xh * H + yl] + (1 - dx) * v[xl * H + yl];
+		Vector2 myh = dx * v[xh * H + yh] + (1 - dx) * v[xl * H + yh];
+		return dy * myh + (1 - dy) * myl;
 	}
 
-	void advect(Vector2 *result, float dt=1) {
-		for (uint x = 0;x < W;++x)
-		for (uint y = 0;y < H;++y) {
-			Vector2 vel = v[x*H+y];
-			result[x*H+y] = lerp_index(Vector2(x-vel.x,y-vel.y))*dt;
-		}
+	void advect(Vector2 *result, float dt = 1) {
+		for (uint x = 0; x < W; ++x)
+			for (uint y = 0; y < H; ++y) {
+				Vector2 vel = v[x * H + y];
+				result[x * H + y] =
+					lerp_index(Vector2(x - vel.x, y - vel.y)) * dt;
+			}
 	}
 
 	void divergence(float *result) {
-		for (uint x = 1;x < W-1;++x)
-		for (uint y = 1;y < H-1;++y) {
-			result[x*H+y] = (v[(x+1)*H+y].x - v[(x-1)*H+y].x + v[x*H+y+1].y - v[x*H+y-1].y) / 2;
-		}
+		for (uint x = 1; x < W - 1; ++x)
+			for (uint y = 1; y < H - 1; ++y) {
+				result[x * H + y] =
+					(v[(x + 1) * H + y].x - v[(x - 1) * H + y].x +
+					 v[x * H + y + 1].y - v[x * H + y - 1].y) /
+					2;
+			}
 	}
 
-	void diffuse(float dt=1) {
-		float alpha = 1/(viscosity*dt);
-		Vector2 *b = new Vector2[W*H];
-		memcpy(b, v, W*H*sizeof(Vector2));
-		jacobi(v, b, alpha, 4+alpha);
+	void diffuse(float dt = 1) {
+		float alpha = 1 / (viscosity * dt);
+		Vector2 *b = new Vector2[W * H];
+		memcpy(b, v, W * H * sizeof(Vector2));
+		jacobi(v, b, alpha, 4 + alpha);
 		delete[] b;
 	}
-	
+
 	void updatePressure() {
-		float *dvg = new float[W*H];
+		float *dvg = new float[W * H];
 		divergence(dvg);
 		jacobi(p, dvg, -1, 4);
 	}
@@ -167,8 +187,9 @@ int main() {
 
 	if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
 		printf("error initializing SDL: %s\n", SDL_GetError());
-	SDL_Window *window = SDL_CreateWindow("GAME", SDL_WINDOWPOS_CENTERED,
-										  SDL_WINDOWPOS_CENTERED, pb.width(), pb.height(), 0);
+	SDL_Window *window =
+		SDL_CreateWindow("GAME", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+						 pb.width(), pb.height(), 0);
 
 	SDL_Renderer *renderer =
 		SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
@@ -181,8 +202,9 @@ int main() {
 	}
 
 	// Create a texture to render the buffer to
-	SDL_Texture *texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888,
-											 SDL_TEXTUREACCESS_STREAMING, pb.width(), pb.height());
+	SDL_Texture *texture =
+		SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888,
+						  SDL_TEXTUREACCESS_STREAMING, pb.width(), pb.height());
 	if (!texture) {
 		std::cerr << "SDL_CreateTexture Error: " << SDL_GetError() << std::endl;
 		SDL_DestroyRenderer(renderer);
@@ -192,11 +214,11 @@ int main() {
 	}
 
 	// Create a pixel buffer
-	uint32_t *buffer = new uint32_t[pb.width()*pb.height()];
+	uint32_t *buffer = new uint32_t[pb.width() * pb.height()];
 
-	for (int i = 20; i < 40;++i)
-	for (int j = 200; j < 220;++j)
-		buffer[i + j*pb.height()] = (255|(255<<8));
+	for (int i = 20; i < 40; ++i)
+		for (int j = 200; j < 220; ++j)
+			buffer[i + j * pb.height()] = (255 | (255 << 8));
 
 	bool running = true;
 	SDL_Event event;
@@ -209,10 +231,12 @@ int main() {
 			}
 		}
 
-		for (int i = 0; i < pb.width();++i)
-		for (int j = 0; j < pb.height();++j)
-			buffer[i + j*pb.height()] += buffer[i+1+(j+1)*pb.height()];
-		SDL_UpdateTexture(texture, nullptr, buffer, pb.width() * sizeof(uint32_t));
+		for (int i = 0; i < pb.width(); ++i)
+			for (int j = 0; j < pb.height(); ++j)
+				buffer[i + j * pb.height()] +=
+					buffer[i + 1 + (j + 1) * pb.height()];
+		SDL_UpdateTexture(texture, nullptr, buffer,
+						  pb.width() * sizeof(uint32_t));
 		SDL_RenderClear(renderer);
 		SDL_RenderCopy(renderer, texture, nullptr, nullptr);
 		SDL_RenderPresent(renderer);
